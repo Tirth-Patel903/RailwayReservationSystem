2729c14060fb40b87a37b46f2b53b27b4dbe1184
Tirth Patel <99315530+Tirth-Patel903@users.noreply.github.com>
2025-01-24 08:35:58 -0500

    Add files via upload

---
+++ Home.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.TrainToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReservationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CancellationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CustomerCareToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TrainToolStripMenuItem, Me.ReservationToolStripMenuItem, Me.CancellationToolStripMenuItem, Me.ReportsToolStripMenuItem, Me.CustomerCareToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(781, 26)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'TrainToolStripMenuItem
+        '
+        Me.TrainToolStripMenuItem.Name = "TrainToolStripMenuItem"
+        Me.TrainToolStripMenuItem.Size = New System.Drawing.Size(91, 22)
+        Me.TrainToolStripMenuItem.Text = "Add Train"
+        '
+        'ReservationToolStripMenuItem
+        '
+        Me.ReservationToolStripMenuItem.Name = "ReservationToolStripMenuItem"
+        Me.ReservationToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.ReservationToolStripMenuItem.Text = "Reservation"
+        '
+        'CancellationToolStripMenuItem
+        '
+        Me.CancellationToolStripMenuItem.Name = "CancellationToolStripMenuItem"
+        Me.CancellationToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.CancellationToolStripMenuItem.Text = "Cancellation"
+        '
+        'ReportsToolStripMenuItem
+        '
+        Me.ReportsToolStripMenuItem.Name = "ReportsToolStripMenuItem"
+        Me.ReportsToolStripMenuItem.Size = New System.Drawing.Size(80, 22)
+        Me.ReportsToolStripMenuItem.Text = "Reports"
+        '
+        'CustomerCareToolStripMenuItem1
+        '
+        Me.CustomerCareToolStripMenuItem1.Name = "CustomerCareToolStripMenuItem1"
+        Me.CustomerCareToolStripMenuItem1.Size = New System.Drawing.Size(135, 22)
+        Me.CustomerCareToolStripMenuItem1.Text = "Customer Care"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(48, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.RailwayReservationSystem.My.Resources.Resources.kashipara
+        Me.PictureBox1.Location = New System.Drawing.Point(648, 39)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(121, 47)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(781, 484)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Home"
+        Me.Text = "Home"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents TrainToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReservationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CancellationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CustomerCareToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
